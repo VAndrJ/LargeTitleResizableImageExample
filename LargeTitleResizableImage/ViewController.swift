75ab50558e4d599b1172bafb68b3e3f6a7dedbd9
VAndrJ <vandrju@gmail.com>
2018-03-01 22:00:10 +0200

    Initial

---
+++ LargeTitleResizableImage/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LargeTitleResizableImage
+//
+//  Created by VAndrJ on 3/1/18.
+//  Copyright © 2018 VAndrJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var userImageView: UIImageView!
+
+    // Force unwrap в данном случае гарантирует, что Вы не забыли UINavigationController
+    override func loadView() {
+        super.loadView()
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        navigationController!.view.addSubview(userImageView)
+        let userImageViewConstraints = [userImageView.topAnchor.constraint(equalTo: navigationController!.navigationBar.topAnchor, constant: 4), userImageView.centerXAnchor.constraint(equalTo: navigationController!.view.centerXAnchor), userImageView.bottomAnchor.constraint(equalTo: navigationController!.navigationBar.bottomAnchor, constant: -4), userImageView.heightAnchor.constraint(equalTo: userImageView.widthAnchor)]
+        navigationController!.view.addConstraints(userImageViewConstraints)
+    }
+}
+
